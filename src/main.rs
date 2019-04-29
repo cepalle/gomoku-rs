f4cb7d661517f19282832a4d4c0ff1531aa66ab3
Cyrille Epalle <cepalle@z1r8p3.le-101.fr>
2019-04-29 16:41:24 +0200

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate cursive;
+
+use cursive::Cursive;
+use cursive::views::{Button, Dialog, DummyView,
+                     LinearLayout, TextView};
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = Cursive::default();
+
+    let mut buttons = LinearLayout::vertical();
+
+    for i in 0..19 {
+        let mut h = LinearLayout::horizontal();
+
+        for j in 0..19 {
+            h = h.child(TextView::new("# "))
+        }
+        buttons = buttons.child(h);
+    }
+
+    siv.add_layer(Dialog::around(LinearLayout::horizontal()
+        .child(buttons)
+        .child(DummyView)
+        .child(Button::new("Quit", Cursive::quit)))
+        .title("Gommoku"));
+
+    siv.run();
 }
